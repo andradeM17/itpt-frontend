e68e72d284ddb61560bb04b3b81017855360031f
andradeM17 <mark.andrade@adaptcentre.ie>
2026-09-23 12:26:51 +0100

    fixed a typo

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,7 @@ function App() {
 
         {mode === "bilingual_to_aligned" && (
           <p>
-            Takes a bilingual text files (English and Irish) and aligns the sentences. The output will be a text file with aligned sentence pairs.
+            Takes bilingual text files (English and Irish) and aligns the sentences. The output will be a text file with aligned sentence pairs.
           </p>
         )}
 
